4aed729153e18fc7b30a0604e9d690ad4fdde21b
Olivier Tournaire <olitour@gmail.com>
2010-04-11 07:20:04 +0000

    [Tiled TIFFs tests] Refactoring (second steps: handle minisblack, rgb-contig, rgb-planar)

--- gil_2/libs/gil/io_new/test/tiff_tiled_macros.hpp
+++ gil_2/libs/gil/io_new/test/tiff_tiled_macros.hpp
@@ -1,37 +1,36 @@
 #ifndef __TIFF_TILED_MACROS_HPP__
 #define __TIFF_TILED_MACROS_HPP__
 
+#include <boost/mpl/vector.hpp>
+#include <boost/gil/gil_all.hpp>
 #include <boost/gil/extension/io_new/tiff_all.hpp>
 
 #include "paths.hpp"
 
 #include <boost/preprocessor/cat.hpp>
 #include <boost/preprocessor/stringize.hpp>
-#include <boost/preprocessor/comparison/greater_equal.hpp>
+#include <boost/preprocessor/tuple/elem.hpp>
+#include <boost/preprocessor/comparison/less.hpp>
 #include <boost/preprocessor/repetition/repeat_from_to.hpp>
 
 typedef boost::gil::tiff_tag tag_t;
 
-#define GENERATE_TILE_STRIP_COMPARISON_RGB_BIT_ALIGNED(z, n, data)\
-    BOOST_AUTO_TEST_CASE( BOOST_PP_CAT(BOOST_PP_CAT(read_tile_and_compare_with_rgb_strip_,n), bit_bit_aligned) )\
+#define GENERATE_TILE_STRIP_COMPARISON_BIT_ALIGNED(z, n, data)\
+    BOOST_AUTO_TEST_CASE( BOOST_PP_CAT(BOOST_PP_CAT(BOOST_PP_CAT(BOOST_PP_CAT(read_tile_and_compare_with_,BOOST_PP_TUPLE_ELEM(2,0,data)),_strip_),n), bit_bit_aligned) )\
     { \
       using namespace std; \
       using namespace boost; \
       using namespace gil; \
-      std::string filename_strip( tiff_in_GM + "tiger-rgb-strip-" + BOOST_PP_STRINGIZE(data) + "-" ); \
-      std::string filename_tile ( tiff_in_GM + "tiger-rgb-tile-"  + BOOST_PP_STRINGIZE(data) + "-" ); \
-      typedef bit_aligned_image3_type< n, n, n, rgb_layout_t >::type image_t; \
-      if(BOOST_PP_GREATER_EQUAL(n, 10)==1) \
-      { \
-        filename_strip = filename_strip + BOOST_PP_STRINGIZE(n) + ".tif"; \
-        filename_tile = filename_tile   + BOOST_PP_STRINGIZE(n) + ".tif"; \
-      } \
-      else \
-      { \
-        filename_strip = filename_strip + BOOST_PP_STRINGIZE(BOOST_PP_CAT( 0 , n)) + ".tif"; \
-        filename_tile = filename_tile   + BOOST_PP_STRINGIZE(BOOST_PP_CAT( 0 , n)) + ".tif"; \
-      } \
-      image_t img_strip, img_tile; \
+      string filename_strip( tiff_in_GM + "tiger-" + BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(2,0,data)) + "-strip-" + BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(2,1,data))); \
+      string filename_tile ( tiff_in_GM + "tiger-" + BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(2,0,data)) + "-tile-"  + BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(2,1,data))); \
+      string padding(""); \
+      if(BOOST_PP_LESS(n, 10)==1) \
+        padding = "0"; \
+      filename_strip = filename_strip + padding + BOOST_PP_STRINGIZE(n) + ".tif"; \
+      filename_tile  = filename_tile  + padding + BOOST_PP_STRINGIZE(n) + ".tif"; \
+      typedef mpl::vector< bit_aligned_image3_type< n, n, n, rgb_layout_t  >::type \
+                         , bit_aligned_image1_type< n,       gray_layout_t >::type > image_types; \
+      any_image< image_types > img_strip, img_tile; \
       read_image( filename_strip, img_strip, tag_t() ); \
       read_image( filename_tile,  img_tile,  tag_t() ); \
       BOOST_CHECK_EQUAL( equal_pixels( const_view(img_strip), const_view(img_tile) ), true); \
--- gil_2/libs/gil/io_new/test/tiff_tiled_minisblack_test.cpp
+++ gil_2/libs/gil/io_new/test/tiff_tiled_minisblack_test.cpp
@@ -4,47 +4,13 @@
  *
  *******************************************************/
 
-#include <boost/gil/extension/io_new/tiff_all.hpp>
-#include "paths.hpp"
-
 #include <boost/test/unit_test.hpp>
 
-#include <boost/preprocessor/cat.hpp>
-#include <boost/preprocessor/stringize.hpp>
-#include <boost/preprocessor/comparison/greater_equal.hpp>
-#include <boost/preprocessor/repetition/repeat_from_to.hpp>
-
-using namespace std;
-using namespace boost;
-using namespace gil;
-
-typedef tiff_tag tag_t;
+#include "tiff_tiled_macros.hpp"
 
 BOOST_AUTO_TEST_SUITE( tiff_test )
 
-#define GENERATE_TILE_STRIP_COMPARISON_MINISBLACK_BIT_ALIGNED(z, n, data)\
-    BOOST_AUTO_TEST_CASE( BOOST_PP_CAT(BOOST_PP_CAT(read_tile_and_compare_with_minisblack_strip_,n), bit_bit_aligned) )\
-    { \
-      std::string filename_strip( tiff_in_GM + "tiger-minisblack-strip-" ); \
-      std::string filename_tile ( tiff_in_GM + "tiger-minisblack-tile-"  ); \
-      typedef bit_aligned_image1_type< n, gray_layout_t >::type image_t; \
-      if(BOOST_PP_GREATER_EQUAL(n, 10)==1) \
-      { \
-        filename_strip = filename_strip + BOOST_PP_STRINGIZE(n) + ".tif"; \
-        filename_tile = filename_tile   + BOOST_PP_STRINGIZE(n) + ".tif"; \
-      } \
-      else \
-      { \
-        filename_strip = filename_strip + BOOST_PP_STRINGIZE(BOOST_PP_CAT( 0 , n)) + ".tif"; \
-        filename_tile = filename_tile   + BOOST_PP_STRINGIZE(BOOST_PP_CAT( 0 , n)) + ".tif"; \
-      } \
-      image_t img_strip, img_tile; \
-      read_image( filename_strip, img_strip, tag_t() ); \
-      read_image( filename_tile,  img_tile,  tag_t() ); \
-      BOOST_CHECK_EQUAL( equal_pixels( const_view(img_strip), const_view(img_tile) ), true); \
-    } \
-
-BOOST_PP_REPEAT_FROM_TO(1, 33, GENERATE_TILE_STRIP_COMPARISON_MINISBLACK_BIT_ALIGNED, ~)
-GENERATE_TILE_STRIP_COMPARISON_MINISBLACK_BIT_ALIGNED(dummy, 64, dummy);
+BOOST_PP_REPEAT_FROM_TO(1, 33, GENERATE_TILE_STRIP_COMPARISON_BIT_ALIGNED, (minisblack,) )
+GENERATE_TILE_STRIP_COMPARISON_BIT_ALIGNED( , 64, (minisblack,) );
 
 BOOST_AUTO_TEST_SUITE_END()
