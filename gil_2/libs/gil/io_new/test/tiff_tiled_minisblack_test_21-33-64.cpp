4c73ab2e3da96458f23f818c8906074a39bb5753
Olivier Tournaire <olitour@gmail.com>
2010-08-09 20:15:39 +0000

    Linux and boost 1.42 compatibility for tiff_tiled_minisblack_test_21-33-64

--- gil_2/libs/gil/io_new/test/tiff_tiled_minisblack_test_21-33-64.cpp
+++ gil_2/libs/gil/io_new/test/tiff_tiled_minisblack_test_21-33-64.cpp
@@ -5,6 +5,7 @@
  *******************************************************/
 
 #include <boost/test/unit_test.hpp>
+#include <boost/cstdint.hpp>
 
 #include "tiff_tiled_macros.hpp"
 
@@ -12,6 +13,7 @@ BOOST_AUTO_TEST_SUITE( tiff_test )
 
 BOOST_PP_REPEAT_FROM_TO(21, 32, GENERATE_TILE_STRIP_COMPARISON_BIT_ALIGNED_MINISBLACK, minisblack )
 //GENERATE_TILE_STRIP_COMPARISON_BIT_ALIGNED_MINISBLACK( , 64, minisblack );
+
 
 BOOST_AUTO_TEST_CASE( read_tile_and_compare_with_minisblack_strip_32 )
 {
@@ -23,7 +25,7 @@ BOOST_AUTO_TEST_CASE( read_tile_and_compare_with_minisblack_strip_32 )
     string filename_tile ( tiff_in_GM + "tiger-minisblack-tile-32.tif"  );
 
     typedef pixel< unsigned int, gray_layout_t > gray32_pixel_t;
-    image< gray32_pixel_t > img_strip, img_tile;
+    image< gray32_pixel_t, false > img_strip, img_tile;
 
     read_image( filename_strip, img_strip, tag_t() );
     read_image( filename_tile,  img_tile,  tag_t() );
@@ -40,8 +42,9 @@ BOOST_AUTO_TEST_CASE( read_tile_and_compare_with_minisblack_strip_64 )
     string filename_strip( tiff_in_GM + "tiger-minisblack-strip-64.tif" );
     string filename_tile ( tiff_in_GM + "tiger-minisblack-tile-64.tif"  );
 
-    typedef pixel< unsigned __int64, gray_layout_t > gray64_pixel_t;
-    image< gray64_pixel_t > img_strip, img_tile;
+    //typedef pixel< unsigned __int64, gray_layout_t > gray64_pixel_t;
+    typedef pixel< uint64_t, gray_layout_t > gray64_pixel_t;
+    image< gray64_pixel_t, false > img_strip, img_tile;
 
     read_image( filename_strip, img_strip, tag_t() );
     read_image( filename_tile,  img_tile,  tag_t() );
