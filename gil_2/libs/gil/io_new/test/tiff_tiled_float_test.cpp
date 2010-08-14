db77dabee2c4b93a3f07ccb56b3e86fa60961912
Olivier Tournaire <olitour@gmail.com>
2010-08-14 07:36:47 +0000

    Add some tiled TIFF float tests

--- gil_2/libs/gil/io_new/test/tiff_tiled_float_test.cpp
+++ gil_2/libs/gil/io_new/test/tiff_tiled_float_test.cpp
@@ -17,12 +17,38 @@ typedef tiff_tag tag_t;
 
 BOOST_AUTO_TEST_SUITE( tiff_test )
 
+BOOST_AUTO_TEST_CASE( read_minisblack_float_tile_and_strip32 )
+{
+    std::string filename_strip( tiff_in_GM + "tiger-minisblack-float-strip-32.tif" );
+    std::string filename_tile ( tiff_in_GM + "tiger-minisblack-float-tile-32.tif"  );
+
+    gray32f_image_t img_strip, img_tile;
+
+    read_image( filename_strip, img_strip, tag_t() );
+    read_image( filename_tile,  img_tile,  tag_t() );
+
+    BOOST_CHECK_EQUAL( equal_pixels( const_view(img_strip), const_view(img_tile) ), true);
+}
+
 BOOST_AUTO_TEST_CASE( read_rgb_float_tile_and_strip32 )
 {
     std::string filename_strip( tiff_in_GM + "tiger-rgb-float-strip-planar-32.tif" );
     std::string filename_tile ( tiff_in_GM + "tiger-rgb-float-tile-planar-32.tif"  );
 
     rgb32f_image_t img_strip, img_tile;
+
+    read_image( filename_strip, img_strip, tag_t() );
+    read_image( filename_tile,  img_tile,  tag_t() );
+
+    BOOST_CHECK_EQUAL( equal_pixels( const_view(img_strip), const_view(img_tile) ), true);
+}
+
+BOOST_AUTO_TEST_CASE( read_minisblack_float_tile_and_strip64 )
+{
+    std::string filename_strip( tiff_in_GM + "tiger-minisblack-float-strip-64.tif" );
+    std::string filename_tile ( tiff_in_GM + "tiger-minisblack-float-tile-64.tif"  );
+
+    gil::detail::gray64f_image_t img_strip, img_tile;
 
     read_image( filename_strip, img_strip, tag_t() );
     read_image( filename_tile,  img_tile,  tag_t() );
