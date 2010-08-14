c1ffefbf834156efa51fa76162792341fa39612e
Olivier Tournaire <olitour@gmail.com>
2010-08-14 08:00:13 +0000

    Move and add tiled TIFF float tests

--- gil_2/libs/gil/io_new/test/tiff_tiled_float_test.cpp
+++ gil_2/libs/gil/io_new/test/tiff_tiled_float_test.cpp
@@ -30,19 +30,6 @@ BOOST_AUTO_TEST_CASE( read_minisblack_float_tile_and_strip32 )
     BOOST_CHECK_EQUAL( equal_pixels( const_view(img_strip), const_view(img_tile) ), true);
 }
 
-BOOST_AUTO_TEST_CASE( read_rgb_float_tile_and_strip32 )
-{
-    std::string filename_strip( tiff_in_GM + "tiger-rgb-float-strip-planar-32.tif" );
-    std::string filename_tile ( tiff_in_GM + "tiger-rgb-float-tile-planar-32.tif"  );
-
-    rgb32f_image_t img_strip, img_tile;
-
-    read_image( filename_strip, img_strip, tag_t() );
-    read_image( filename_tile,  img_tile,  tag_t() );
-
-    BOOST_CHECK_EQUAL( equal_pixels( const_view(img_strip), const_view(img_tile) ), true);
-}
-
 BOOST_AUTO_TEST_CASE( read_minisblack_float_tile_and_strip64 )
 {
     std::string filename_strip( tiff_in_GM + "tiger-minisblack-float-strip-64.tif" );
@@ -56,10 +43,49 @@ BOOST_AUTO_TEST_CASE( read_minisblack_float_tile_and_strip64 )
     BOOST_CHECK_EQUAL( equal_pixels( const_view(img_strip), const_view(img_tile) ), true);
 }
 
+BOOST_AUTO_TEST_CASE( read_rgb_float_tile_and_strip_planar32 )
+{
+    std::string filename_strip( tiff_in_GM + "tiger-rgb-float-strip-planar-32.tif" );
+    std::string filename_tile ( tiff_in_GM + "tiger-rgb-float-tile-planar-32.tif"  );
+
+    rgb32f_image_t img_strip, img_tile;
+
+    read_image( filename_strip, img_strip, tag_t() );
+    read_image( filename_tile,  img_tile,  tag_t() );
+
+    BOOST_CHECK_EQUAL( equal_pixels( const_view(img_strip), const_view(img_tile) ), true);
+}
+
+BOOST_AUTO_TEST_CASE( read_rgb_float_tile_and_strip_contig32 )
+{
+    std::string filename_strip( tiff_in_GM + "tiger-rgb-float-strip-contig-32.tif" );
+    std::string filename_tile ( tiff_in_GM + "tiger-rgb-float-tile-contig-32.tif"  );
+
+    rgb32f_image_t img_strip, img_tile;
+
+    read_image( filename_strip, img_strip, tag_t() );
+    read_image( filename_tile,  img_tile,  tag_t() );
+
+    BOOST_CHECK_EQUAL( equal_pixels( const_view(img_strip), const_view(img_tile) ), true);
+}
+
 BOOST_AUTO_TEST_CASE( read_rgb_float_tile_and_strip64 )
 {
     std::string filename_strip( tiff_in_GM + "tiger-rgb-float-strip-planar-64.tif" );
     std::string filename_tile ( tiff_in_GM + "tiger-rgb-float-tile-planar-64.tif"  );
+
+    gil::detail::rgb64f_image_t img_strip, img_tile;
+
+    read_image( filename_strip, img_strip, tag_t() );
+    read_image( filename_tile,  img_tile,  tag_t() );
+
+    BOOST_CHECK_EQUAL( equal_pixels( const_view(img_strip), const_view(img_tile) ), true);
+}
+
+BOOST_AUTO_TEST_CASE( read_rgb_float_tile_and_strip_contig64 )
+{
+    std::string filename_strip( tiff_in_GM + "tiger-rgb-float-strip-contig-64.tif" );
+    std::string filename_tile ( tiff_in_GM + "tiger-rgb-float-tile-contig-64.tif"  );
 
     gil::detail::rgb64f_image_t img_strip, img_tile;
 
--- gil_2/libs/gil/io_new/test/tiff_tiled_rgb_contig_test_21-31_32_64.cpp
+++ gil_2/libs/gil/io_new/test/tiff_tiled_rgb_contig_test_21-31_32_64.cpp
@@ -48,39 +48,4 @@ BOOST_AUTO_TEST_CASE( read_tile_and_compare_with_rgb_contig_strip_64 )
     BOOST_CHECK_EQUAL( equal_pixels( const_view(img_strip), const_view(img_tile) ), true); \
 }
 
-// TODO: move in a rgb_float test file
-BOOST_AUTO_TEST_CASE( read_tile_and_compare_with_rgb_float_contig_strip_32 )
-{
-    using namespace std;
-    using namespace boost;
-    using namespace gil;
-
-    string filename_strip( tiff_in_GM + "tiger-rgb-float-tile-contig-32.tif" );
-    string filename_tile ( tiff_in_GM + "tiger-rgb-float-strip-contig-32.tif" );
-
-    rgb32f_image_t img_strip, img_tile;
-
-    read_image( filename_strip, img_strip, tag_t() );
-    read_image( filename_tile,  img_tile,  tag_t() );
-
-    BOOST_CHECK_EQUAL( equal_pixels( const_view(img_strip), const_view(img_tile) ), true); \
-}
-
-BOOST_AUTO_TEST_CASE( read_tile_and_compare_with_rgb_float_contig_strip_64 )
-{
-    using namespace std;
-    using namespace boost;
-    using namespace gil;
-
-    string filename_strip( tiff_in_GM + "tiger-rgb-float-tile-contig-64.tif" );
-    string filename_tile ( tiff_in_GM + "tiger-rgb-float-strip-contig-64.tif" );
-
-    gil::detail::rgb64f_image_t img_strip, img_tile;
-
-    read_image( filename_strip, img_strip, tag_t() );
-    read_image( filename_tile,  img_tile,  tag_t() );
-
-    BOOST_CHECK_EQUAL( equal_pixels( const_view(img_strip), const_view(img_tile) ), true); \
-}
-
 BOOST_AUTO_TEST_SUITE_END()
