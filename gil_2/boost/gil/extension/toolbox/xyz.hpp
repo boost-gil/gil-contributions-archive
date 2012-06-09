56702ea48df2ea1ee95822398bf00cf98098ab5a
Davide Anastasia <davide.anastasia@gmail.com>
2012-06-09 00:06:22 +0000

    Improved support for CIE XYZ Colorspace

--- gil_2/boost/gil/extension/toolbox/xyz.hpp
+++ gil_2/boost/gil/extension/toolbox/xyz.hpp
@@ -9,9 +9,10 @@
 #define GIL_XYZ_H
 
 ////////////////////////////////////////////////////////////////////////////////////////
-/// \file
+/// \file xyz.hpp
 /// \brief Support for CIE XYZ color space
 /// \author Chung-Lin Wen \n
+/// \author Davide Anastasia \n
 ////////////////////////////////////////////////////////////////////////////////////////
 
 #include <boost/cast.hpp>
@@ -41,68 +42,64 @@ typedef mpl::vector3< xyz_color_space::x_t
 /// \ingroup LayoutModel
 typedef layout<xyz_t> xyz_layout_t;
 
-
 GIL_DEFINE_ALL_TYPEDEFS( 32f, xyz );
 
 /// \ingroup ColorConvert
 /// \brief RGB to XYZ
+/// \ref http://www.brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html
+/// \note rgb_t is assumed to be sRGB D65
 template <>
 struct default_color_converter_impl< rgb_t, xyz_t >
 {
-   template <typename P1, typename P2>
-   void operator()( const P1& src, P2& dst ) const
-   {
-      using namespace xyz_color_space;
+private:
+    GIL_FORCEINLINE
+    bits32f inverse_companding(bits32f sample) const
+    {
+        if ( sample > 0.04045f )
+        {
+            return powf((( sample + 0.055f ) / 1.055f ), 2.4f);
+        }
+        else
+        {
+            return ( sample / 12.92f );
+        }
+    }
 
-      // only bits32f for xyz is supported
-      bits32f temp_red   = channel_convert<bits32f>( get_color( src, red_t()   ));
-      bits32f temp_green = channel_convert<bits32f>( get_color( src, green_t() ));
-      bits32f temp_blue  = channel_convert<bits32f>( get_color( src, blue_t()  ));
+public:
+    template <typename P1, typename P2>
+    void operator()( const P1& src, P2& dst ) const
+    {
+        using namespace xyz_color_space;
 
-      bits32f normalized_r = temp_red / 255.f;
-      bits32f normalized_g = temp_green / 255.f;
-      bits32f normalized_b = temp_blue / 255.f;
+        bits32f red(
+                    inverse_companding(
+                        channel_convert<bits32f>( get_color( src, red_t() ))
+                        )
+                    );
+        bits32f green(
+                    inverse_companding(
+                        channel_convert<bits32f>( get_color( src, green_t() ))
+                        )
+                    );
+        bits32f blue(
+                    inverse_companding(
+                        channel_convert<bits32f>( get_color( src, blue_t() ))
+                        )
+                    );
 
-      if( normalized_r > 0.04045f )
-      {
-         normalized_r = pow((( normalized_r + 0.055f ) / 1.055f ), 2.4f );
-      }
-      else
-      {
-         normalized_r /= 12.92f;
-      }
-
-      if( normalized_g > 0.04045f )
-      {
-         normalized_g = pow((( normalized_g + 0.055f ) / 1.055f ), 2.4f );
-      }
-      else
-      {
-         normalized_g /= 12.92f;
-      }
-      
-      if( normalized_b > 0.04045f )
-      {
-         normalized_b = pow((( normalized_b + 0.055f ) / 1.055f ), 2.4f );
-      }
-      else
-      {
-         normalized_b /= 12.92f;
-      }
-
-      normalized_r *= 100.f;
-      normalized_g *= 100.f;
-      normalized_b *= 100.f;
-
-      bits32f x, y, z;
-      x = normalized_r * 0.4124f + normalized_g * 0.3576f + normalized_b * 0.1805f;
-      y = normalized_r * 0.2126f + normalized_g * 0.7152f + normalized_b * 0.0722f;
-      z = normalized_r * 0.0193f + normalized_g * 0.1192f + normalized_b * 0.9505f;
-
-      get_color( dst, x_t() ) = x;
-      get_color( dst, y_t() ) = y;
-      get_color( dst, z_t() ) = z;
-   }
+        get_color( dst, x_t() ) =
+                red * 0.4124564f +
+                green * 0.3575761f +
+                blue * 0.1804375f;
+        get_color( dst, y_t() ) =
+                red * 0.2126729f +
+                green * 0.7151522f +
+                blue * 0.0721750f;
+        get_color( dst, z_t() ) =
+                red * 0.0193339f +
+                green * 0.1191920f +
+                blue * 0.9503041f;
+    }
 };
 
 /// \ingroup ColorConvert
@@ -110,62 +107,51 @@ struct default_color_converter_impl< rgb_t, xyz_t >
 template <>
 struct default_color_converter_impl<xyz_t,rgb_t>
 {
-   template <typename P1, typename P2>
-   void operator()( const P1& src, P2& dst) const
-   {
-      using namespace xyz_color_space;
+private:
+    GIL_FORCEINLINE
+    bits32f companding(bits32f sample) const
+    {
+        if ( sample > 0.0031308f )
+        {
+            return ( 1.055f * powf( sample, 1.f/2.4f ) - 0.055f );
+        }
+        else
+        {
+            return ( 12.92f * sample );
+        }
+    }
 
-      bits32f x = get_color( src, x_t() );
-      bits32f y = get_color( src, y_t() );
-      bits32f z = get_color( src, z_t() );
+public:
+    template <typename P1, typename P2>
+    void operator()( const P1& src, P2& dst) const
+    {
+        using namespace xyz_color_space;
 
-      bits32f normalized_x = x / 100.f;
-      bits32f normalized_y = y / 100.f;
-      bits32f normalized_z = z / 100.f;
+        // Note: ideally channel_convert should be compiled out, because xyz_t
+        // is bits32f natively only
+        bits32f x( channel_convert<bits32f>( get_color( src, x_t() ) ) );
+        bits32f y( channel_convert<bits32f>( get_color( src, y_t() ) ) );
+        bits32f z( channel_convert<bits32f>( get_color( src, z_t() ) ) );
 
-      bits32f result_r = normalized_x *  3.2406f + normalized_y * -1.5372f + normalized_z * -0.4986f;
-      bits32f result_g = normalized_x * -0.9689f + normalized_y *  1.8758f + normalized_z *  0.0415f;
-      bits32f result_b = normalized_x *  0.0557f + normalized_y * -0.2040f + normalized_z *  1.0570f;
-
-      if ( result_r > 0.0031308f ) 
-      {
-         result_r = 1.055f * pow( result_r, 1.f/2.4f ) - 0.055f;
-      }
-      else                     
-      {
-         result_r = 12.92f * result_r;
-      }
-
-      if ( result_g > 0.0031308f ) 
-      {
-         result_g = 1.055f * pow( result_g, 1.f/2.4f ) - 0.055f;
-      }
-      else                     
-      {
-         result_g = 12.92f * result_g;
-      }
-
-      if ( result_b > 0.0031308f ) 
-      {
-         result_b = 1.055f * pow( result_b, 1.f/2.4f ) - 0.055f;
-      }
-      else                     
-      {
-         result_b = 12.92f * result_b;
-      }
-
-      bits32f red, green, blue;
-      red = result_r * 255.f;
-      green = result_g * 255.f;
-      blue = result_b * 255.f;
-
-      get_color(dst,red_t())  =
-         channel_convert<typename color_element_type<P2, red_t>::type>( red );
-      get_color(dst,green_t())=
-         channel_convert<typename color_element_type<P2, green_t>::type>( green );
-      get_color(dst,blue_t()) =
-         channel_convert<typename color_element_type<P2, blue_t>::type>( blue );
-   }
+        get_color(dst,red_t())  =
+                channel_convert<typename color_element_type<P2, red_t>::type>(
+                    companding( x *  3.2404542f +
+                                y * -1.5371385f +
+                                z * -0.4985314f )
+                    );
+        get_color(dst,green_t()) =
+                channel_convert<typename color_element_type<P2, green_t>::type>(
+                    companding( x * -0.9692660f +
+                                y *  1.8760108f +
+                                z *  0.0415560f )
+                    );
+        get_color(dst,blue_t()) =
+                channel_convert<typename color_element_type<P2, blue_t>::type>(
+                    companding( x *  0.0556434f +
+                                y * -0.2040259f +
+                                z *  1.0572252f )
+                    );
+    }
 };
 
 } }  // namespace boost::gil
